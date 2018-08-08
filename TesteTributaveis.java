d18c31c76b8d90f737b5d2184a87998a647d557b
Guilherme Lima <33007095+guilsilva@users.noreply.github.com>
2018-08-08 11:25:49 -0300

    Exceptions-Em-CC

---
+++ TesteTributaveis.java
@@ -0,0 +1,18 @@
+
+public class TesteTributaveis {
+
+	public static void main(String[] args) {
+		ContaCorrente cc = new ContaCorrente(222, 333);
+		cc.deposita(100);
+		
+		SeguroVida sv = new SeguroVida();
+		
+		CalculadorImposto calc = new CalculadorImposto();
+		calc.registra(cc);
+		calc.registra(sv);
+		
+		System.out.println(calc.getTotalImposto());
+
+	}
+
+}
